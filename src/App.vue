600611542b1ddddf0820c11840027921759fcc8f
Sef-99 <enriquecaballero1999@gmail.com>
2024-04-17 09:17:17 -0400

    add initial project with vue router set up

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import PokemonPageVue from './modules/pokemon/pages/PokemonPage.vue';
+import aboutPage from './modules/pokemon/pages/aboutPage.vue';
+import listPageVue from './modules/pokemon/pages/listPage.vue';
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
