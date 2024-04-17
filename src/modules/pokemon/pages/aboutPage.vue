bfe638987ce340542a530e03408f8ef28153cb35
Sef-99 <159728978+Sef-99@users.noreply.github.com>
2024-04-17 12:20:40 -0400

    add lifecyle hook on aboutpage

--- src/modules/pokemon/pages/aboutPage.vue
+++ src/modules/pokemon/pages/aboutPage.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>About Page</h1>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{ name }}</h2>
+  </div>
+  <button @click="onChangeName">Change Name</button>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Enrique",
+    };
+  },
+
+  methods: {
+    onChangeName() {
+      this.name = "Melissa";
+    },
+  },
+
+  beforeCreate() {},
+  created() {},
+};
+</script>
